bf5a5b756d6ee9ea374e7898b8603a22b63d9d8e
Daniel Castro García <dandev237@gmail.com>
2017-02-11 03:22:20 +0100

    Initial commit

---
+++ Source/InfiniteCaverns/InfiniteCavernsGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "InfiniteCavernsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINITECAVERNS_API AInfiniteCavernsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
